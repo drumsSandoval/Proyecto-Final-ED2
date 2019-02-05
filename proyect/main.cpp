58e1bcd568f420c624604828cdf206e009196d0b
Oscar Sandoval <oscarsandoval@macbook-air-de-oscar.local>
2019-02-04 19:09:45 -0600

    Initial Commit

---
+++ proyect/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  proyect
+//
+//  Created by Oscar Sandoval on 2/4/19.
+//  Copyright © 2019 Oscar Sandoval. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
